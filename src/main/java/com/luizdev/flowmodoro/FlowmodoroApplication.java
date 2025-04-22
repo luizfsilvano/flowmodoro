db0d1665cfa3004932a64a68787796a3dc9669df
luizfsilvano <luizfsilvano@gmail.com>
2025-04-21 23:26:15 -0300

    :tada: init: Commit inicial! Arquitetura inicial definida

---
+++ src/main/java/com/luizdev/flowmodoro/FlowmodoroApplication.java
@@ -0,0 +1,13 @@
+package com.luizdev.flowmodoro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowmodoroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowmodoroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luizdev/flowmodoro/FlowmodoroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizdev.flowmodoro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowmodoroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
